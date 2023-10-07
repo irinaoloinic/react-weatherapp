4b4986fdbfe69014ed092af9f16f086d28c0134d
irinaoloinic <133090430+irinaoloinic@users.noreply.github.com>
2023-10-07 22:43:46 +0200

    adjusted bubel dependency

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="Weather-app-wrapper">
+      <header>
       <h1>Weather app </h1>
+      </header>
+      
       <footer> This project is coded by <a href="https://github.com/irinaoloinic?tab=repositories" target="blank">Irina Oloinic </a>  and is 
       <a href="https://github.com/irinaoloinic/react-weatherapp" target="_blank"> open-sourced on Github</a>
       </footer>
