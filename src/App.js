0b5febf98bb27cc24b4d47bfacd956aeebf01f13
irinaoloinic <133090430+irinaoloinic@users.noreply.github.com>
2023-10-07 22:18:48 +0200

    added the footer of the page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="Weather-app">
       <h1>Weather app </h1>
+      <footer> This project is coded by <a href="https://github.com/irinaoloinic?tab=repositories" target="blank">Irina Oloinic </a>  and is 
+      <a href="https://github.com/irinaoloinic/react-weatherapp" target="_blank"> open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
