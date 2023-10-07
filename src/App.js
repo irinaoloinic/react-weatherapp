e1b79abed87b75e0a377ebae6211e5cfb41e4b78
irinaoloinic <133090430+irinaoloinic@users.noreply.github.com>
2023-10-07 22:21:25 +0200

    changed class name

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="Weather-app">
+    <div className="Weather-app-wrapper">
       <h1>Weather app </h1>
       <footer> This project is coded by <a href="https://github.com/irinaoloinic?tab=repositories" target="blank">Irina Oloinic </a>  and is 
       <a href="https://github.com/irinaoloinic/react-weatherapp" target="_blank"> open-sourced on Github</a>
