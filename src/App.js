d69f6ef1e61af818e0463f3fa837cd041e584d67
irinaoloinic <133090430+irinaoloinic@users.noreply.github.com>
2023-10-08 14:33:58 +0200

    added the basic styling for weather.css

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <header>
         <Weather />
-      <h1>Weather app </h1>
-      </header>
-      
       <footer> This project is coded by <a href="https://github.com/irinaoloinic?tab=repositories" target="blank">Irina Oloinic </a>  and is 
       <a href="https://github.com/irinaoloinic/react-weatherapp" target="blank"> open-sourced on Github</a>
       </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -8,16 +8,16 @@ function Weather(){
     <form>
       <div className="row">
        <div className="col-9">
-        <input type="search" placeholder="Enter a city.." className="form-control"/>
+        <input type="search" placeholder="Enter a city.."  className="form-control" autoFocus="on"/>
        </div>
        <div className="col-3">
-      <input type="submit" value="Search" className="btn btn-primary"/>
+      <input type="submit" value="Search" className="btn btn-primary w-100"/>
       </div>
       </div>
     </form>
 
     <h1>Barcelona</h1>
-     <div className="row">
+     <div className="row mt-3">
       <div className="col-6">
        <ul>
         <li>Last updated: Monday 13:17</li>
@@ -29,7 +29,8 @@ function Weather(){
 
        <div className="col-6">
         <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" alt="weather-icon" />
-         6˚C 
+       <span className="temperature">6 </span>
+       <span className="unit" > ˚C</span>
        </div>
 
      </div>
