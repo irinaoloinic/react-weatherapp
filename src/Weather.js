4299e76a2a7cf20e717b60752f2d3516369af937
irinaoloinic <133090430+irinaoloinic@users.noreply.github.com>
2023-10-09 09:31:14 +0200

    integrated APIs

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-function Weather(){
-  return(
+function Weather(props){
+  
+  const [weatherData, setweatherData] = useState({ready:false});
+  
+
+  function handleResponse(response){
+    console.log(response.data);
+    setweatherData({
+      ready:true,
+      temperature:response.data.temperature.current,
+      wind: response.data.wind.speed,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      city: response.data.city,
+      time: response.data.time,
+      iconUrl:"http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"});
+      
+  
+    }
+
+if (weatherData.ready){
+return(
     <div className="Weather"> 
     
     <form>
@@ -16,25 +37,31 @@ function Weather(){
       </div>
     </form>
 
-    <h1>Barcelona</h1>
+    <h1>{weatherData.city}</h1>
      <div className="row mt-3">
       <div className="col-6">
        <ul>
-        <li>Last updated: Monday 13:17</li>
-        <li>Clear sky</li>
-        <li>Humidity: 77%</li>
-        <li>Wind speed: 3km/h</li>
+        <li>Last updated: {weatherData.time}</li>
+        <li className="text-capitalize">{weatherData.description}</li>
+        <li>Humidity: {weatherData.humidity}%</li>
+        <li>Wind speed: {weatherData.wind} km/h</li>
       </ul>
        </div>
 
        <div className="col-6">
-        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" alt="weather-icon" />
-       <span className="temperature">6 </span>
+        <img src={weatherData.iconUrl} alt="weather-icon" />
+       <span className="temperature">{Math.round(weatherData.temperature)} </span>
        <span className="unit" > ˚C</span>
        </div>
-
      </div>
   </div>
-  )
+  );
+} else{
+  const apiKey="37f6a94ba46fa4c11df96b5390cdt21o";
+  let city="Barcelona";
+  let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
+  return "Loading...";
+ } 
 }
 export default Weather;
