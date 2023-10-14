8ec82eebbc4268cefee2f982c0f548689db6515e
irinaoloinic <133090430+irinaoloinic@users.noreply.github.com>
2023-10-14 18:55:39 +0200

    fixed bug showing the day correctly

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -12,7 +12,7 @@ function minTemperature(){
   return `${temperature}˚`
 }
  function day(){
-  let date = new Date(props.data.temperature.day * 1000 );
+  let date = new Date(props.data.time * 1000 );
   let day = date.getDay();
 
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
